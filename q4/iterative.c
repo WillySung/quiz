0fcc6efebc53d1e8f2be6b88a2cc1b4ad67b9936
WillySung <wanny820514@gmail.com>
2015-10-17 22:42:41 +0800

    if the previous sum are +, which is then useful for the current element, or if it is -, why not start the sub array from current element? We can use an int to store the max sum we have got, just scan the whole array, the result is easily found.

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,26 @@
 /* FIXME: Implement! */
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+int maxSubArray(int A[], int n)
+{
+    if(n<=0) return 0;
+    int maxSum = A[0], curSum = A[0];
+    for(int i=1; i<n; i++) {
+        curSum =  curSum<=0 ? A[i] : A[i]+curSum;
+        if(maxSum<curSum) maxSum=curSum;
+    }
+    return maxSum;
+}
 
 int main()
 {
+    int array[9] =  {-2,1,-3,4,-1,2,1,-5,4};
+
+    int len = sizeof(array)/sizeof(array[0]);
+    int result = maxSubArray(array, len);
+    printf("%d\n",result);
+
     return 0;
 }
