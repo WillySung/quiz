05e41b6fbeec0e0deea075ebc3f7a8ffd3bc5d79
WillySung <wanny820514@gmail.com>
2015-10-17 12:22:17 +0800

    add iterative algorithm

--- q3/iterative.c
+++ q3/iterative.c
@@ -1,6 +1,81 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
 
+// Maximum stack size
+#define MAX_SIZE 100
+
+
+/* A binary tree TreeNode has data, pointer to left child
+   and a pointer to right child */
+struct TreeNode
+{
+     int data;
+     struct TreeNode* left;
+     struct TreeNode* right;
+};
+ 
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct TreeNode *newNode(int data)
+{
+     struct TreeNode *node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+     node->data = data;
+     node->left = NULL;
+     node->right = NULL;
+ 
+     return(node);
+}
+
+// An iterative process to print preorder traversal of Binary tree
+void flatten(struct TreeNode *root)
+{
+    
+     struct TreeNode *stack[MAX_SIZE];
+     int top = -1;
+     struct TreeNode *ptr = root;
+
+     // Base Case
+     if (ptr == NULL)
+        return;
+
+     do
+     {
+        printf("%d ", ptr->data); // print the node
+        if (ptr->right)
+            stack[++top] = ptr->right; 
+        if (ptr->left)
+            stack[++top] = ptr->left; 
+
+        /* Pop a pointer */
+        if (top == -1)
+               break; // we are done
+ 
+        ptr = stack[top--];
+     } while((ptr != NULL) || (top > -1));
+}
+
+/* Driver program to test above functions*/
 int main()
 {
-    return 0;
+    /*
+              1
+             / \
+            2   5
+           / \   \
+          3   4   6
+    */      
+    
+     struct TreeNode *root  = newNode(1);
+     root->left             = newNode(2);
+     root->right           = newNode(5);
+     root->left->left     = newNode(3);
+     root->left->right   = newNode(4);
+     root->right->right  = newNode(6); 
+ 
+     printf("\nflattened binary tree is \n");
+     flatten(root);
+     printf("\n");
+ 
+     return 0;
 }
