0950e62f3fbde880260097e56c09046406a4b166
WillySung <wanny820514@gmail.com>
2015-10-17 20:55:08 +0800

    use while loop

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,30 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char smallest_character(char str[], char c)
+{
+    int i=0;
+    while(str[i]!='\0') {
+        if(str[i]>c)
+            return str[i];
+        i++;
+    }
+    return str[0];
+}
 
 int main()
 {
+    char a[20] = {'c','f','j','p','v'};
+    char b[20] = {'c','c','k'};
+    char ans=smallest_character(a,'a');
+    printf("%c\n", ans);
+    ans=smallest_character(a,'c');
+    printf("%c\n", ans);
+    ans=smallest_character(a,'z');
+    printf("%c\n", ans);
+    ans=smallest_character(b,'f');
+    printf("%c\n", ans);
     return 0;
 }
--- q3/iterative.c
+++ q3/iterative.c
@@ -8,51 +8,49 @@
 
 /* A binary tree TreeNode has data, pointer to left child
    and a pointer to right child */
-struct TreeNode
-{
-     int data;
-     struct TreeNode* left;
-     struct TreeNode* right;
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
 };
- 
+
 /* Helper function that allocates a new node with the
    given data and NULL left and right pointers. */
 struct TreeNode *newNode(int data)
 {
-     struct TreeNode *node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-     node->data = data;
-     node->left = NULL;
-     node->right = NULL;
- 
-     return(node);
+    struct TreeNode *node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return(node);
 }
 
 // An iterative process to print preorder traversal of Binary tree
 void flatten(struct TreeNode *root)
 {
-    
-     struct TreeNode *stack[MAX_SIZE];
-     int top = -1;
-     struct TreeNode *ptr = root;
 
-     // Base Case
-     if (ptr == NULL)
+    struct TreeNode *stack[MAX_SIZE];
+    int top = -1;
+    struct TreeNode *ptr = root;
+
+    // Base Case
+    if (ptr == NULL)
         return;
 
-     do
-     {
+    do {
         printf("%d ", ptr->data); // print the node
         if (ptr->right)
-            stack[++top] = ptr->right; 
+            stack[++top] = ptr->right;
         if (ptr->left)
-            stack[++top] = ptr->left; 
+            stack[++top] = ptr->left;
 
         /* Pop a pointer */
         if (top == -1)
-               break; // we are done
- 
+            break; // we are done
+
         ptr = stack[top--];
-     } while((ptr != NULL) || (top > -1));
+    } while((ptr != NULL) || (top > -1));
 }
 
 /* Driver program to test above functions*/
@@ -64,18 +62,18 @@ int main()
             2   5
            / \   \
           3   4   6
-    */      
-    
-     struct TreeNode *root  = newNode(1);
-     root->left             = newNode(2);
-     root->right           = newNode(5);
-     root->left->left     = newNode(3);
-     root->left->right   = newNode(4);
-     root->right->right  = newNode(6); 
- 
-     printf("\nflattened binary tree is \n");
-     flatten(root);
-     printf("\n");
- 
-     return 0;
+    */
+
+    struct TreeNode *root  = newNode(1);
+    root->left             = newNode(2);
+    root->right           = newNode(5);
+    root->left->left     = newNode(3);
+    root->left->right   = newNode(4);
+    root->right->right  = newNode(6);
+
+    printf("\nflattened binary tree is \n");
+    flatten(root);
+    printf("\n");
+
+    return 0;
 }
